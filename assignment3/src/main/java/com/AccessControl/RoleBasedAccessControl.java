205793a89402a47f14cbda5623ba1c2ab571112d
William <s194050@student.dtu.dk>
2022-11-16 17:30:29 +0100

    Added Json to project

--- assignment3/src/main/java/com/AccessControl/RoleBasedAccessControl.java
+++ assignment3/src/main/java/com/AccessControl/RoleBasedAccessControl.java
@@ -1,13 +1,14 @@
 package com.AccessControl;
 
 // import java.io.FileNotFoundException;
-
+import org.json.JSONObject;
+import org.json.JSONArray
 class RoleBasedAccessControl extends AccessControl{
     
 
     @Override
     public void loadAccessControlFile(){
-        // JSONArray a = (JSONArray) parser.parse(new FileReader("c:\\exer4-courses.json"));
+         JSONArray a = (JSONArray) parser.parse(new FileReader("c:\\exer4-courses.json"));
 
     }
 
