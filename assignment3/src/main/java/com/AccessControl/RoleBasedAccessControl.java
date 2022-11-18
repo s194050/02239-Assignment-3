0dfaf0580f2f1bd5ecf765666aa648d061a72de6
William Mathiassen <s194050@dtu.dk>
2022-11-18 14:56:55 +0100

    Added JSON.simple for the json parser

--- assignment3/src/main/java/com/AccessControl/RoleBasedAccessControl.java
+++ assignment3/src/main/java/com/AccessControl/RoleBasedAccessControl.java
@@ -2,13 +2,22 @@ package com.AccessControl;
 
 // import java.io.FileNotFoundException;
 import org.json.JSONObject;
-import org.json.JSONArray
+
+import java.io.FileNotFoundException;
+import java.io.FileNotFor;
+impout java.io.IOExceptionndException;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.json.JSONArray;
+import org.json.simple.parser.*;
+
 class RoleBasedAccessControl extends AccessControl{
     
-
     @Override
-    public void loadAccessControlFile(){
-         JSONArray a = (JSONArray) parser.parse(new FileReader("c:\\exer4-courses.json"));
+    public void loadAccessControlFile() throws FileNotFoundException, IOException, ParseException{
+        JSONParser parser = new JSONParser();
+        JSONArray a = (JSONArray) parser.parse(new FileReader("Role_based_access_control.json"));
 
     }
 
